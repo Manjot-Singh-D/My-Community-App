1711f40a432c2747131f64fd443e7149dc357916
Manjotsingh Dhanjal <manjotsinghdhanjal@gmail.com>
2021-01-26 20:08:13 +0530

    deploying

--- server.js
+++ server.js
@@ -4,19 +4,26 @@ const bodyParser = require("body-parser");
 const userRoutes = require("./Routes/userRoutes");
 const routes = require("./Routes/routes.js");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(express.static(path.join("public")));
 
 mongoose.connect(
-  "mongodb+srv://my_community:my_community@cluster0.r2mba.mongodb.net/MyCommunity?retryWrites=true&w=majority",
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r2mba.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 app.use("/api/users", userRoutes);
 app.use("/api/general", routes);
+
+app.use((req, res, next) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => console.log("Server is Running"));
